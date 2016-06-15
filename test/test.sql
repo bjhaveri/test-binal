f06bdf57188169644bc9ac5e3a44d02f42815296
bjhaveri <bjhaveri@starbucks.com>
2016-06-15 14:58:11 -0700

    added s3 endpoint

--- test/test.sql
+++ test/test.sql
@@ -1,6 +1,6 @@
-SELECT * FROM mphd.customer_attributes_weekly WHERE weekstartdate = '${weekToQuery}';
+SELECT * FROM mphd.customer_attributes_weekly WHERE weekstartdate = '${weekToQuery}' LIMIT 5;
 
-UNLOAD('SELECT * FROM source.import_variants;') TO ''
+UNLOAD('SELECT * FROM source.import_variants;') TO '${s3DirToUnload}'
 CREDENTIALS 'aws_iam_role=${REDSHIFT_DB_IAM}'
 DELIMITER AS '|'
 ALLOWOVERWRITE
