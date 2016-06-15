3c78006262de64b3db5714b7e5b023717f466512
bjhaveri <bjhaveri@starbucks.com>
2016-06-15 14:55:12 -0700

    added some query variables

--- test/test.sql
+++ test/test.sql
@@ -1,1 +1,7 @@
-SELECT * FROM source.import_variants;
+SELECT * FROM mphd.customer_attributes_weekly WHERE weekstartdate = '${weekToQuery}';
+
+UNLOAD('SELECT * FROM source.import_variants;') TO ''
+CREDENTIALS 'aws_iam_role=${REDSHIFT_DB_IAM}'
+DELIMITER AS '|'
+ALLOWOVERWRITE
+GZIP;
